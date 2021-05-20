ff3d0f93db9a86c304cada333564fb3d3b3dec3d
mehedi_hasan <mehedi609@gmail.com>
2021-05-20 20:52:46 +0600

    created cars app and added cars html template

--- cars/urls.py
+++ cars/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 
-from pages import views
+from cars import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('about', views.about, name='about'),
-    path('services', views.services, name='services'),
-    path('contact', views.contact, name='contact'),
+    path('', views.cars, name='cars'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def cars(request):
+    return render(request, 'cars/cars.html')
