a8b4382a56baf4525cadf40ae851b3d9abd63e3b
mehedi_hasan <mehedi609@gmail.com>
2021-05-20 20:33:10 +0600

    created cars app

--- carzone/settings.py
+++ carzone/settings.py
@@ -31,13 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'pages.apps.PagesConfig',
+    'cars.apps.CarsConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages.apps.PagesConfig'
 ]
 
 MIDDLEWARE = [
