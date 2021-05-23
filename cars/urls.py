a125ac709e6fec019c1c41ea5cb7ffd779f569f7
mehedi_hasan <mehedi609@gmail.com>
2021-05-24 00:52:07 +0600

    get single car by its id and show the car details. Also get the cars for cars page and display all cars.

--- cars/urls.py
+++ cars/urls.py
@@ -4,4 +4,6 @@ from cars import views
 
 urlpatterns = [
     path('', views.cars, name='cars'),
+    path('<int:_id>', views.car_detail, name='car_detail'),
+    path('search', views.search, name='search'),
 ]
--- cars/views.py
+++ cars/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 
 # Create your views here.
+from cars.models import Car
 
 
 def cars(request):
-    return render(request, 'cars/cars.html')
+    _cars = Car.objects.order_by('-created_date')
+
+    data = {
+        'cars': _cars,
+    }
+
+    return render(request, 'cars/cars.html', data)
+
+
+def car_detail(request, _id):
+    single_car = get_object_or_404(Car, pk=_id)
+
+    data = {
+        'single_car': single_car,
+    }
+
+    return render(request, 'cars/car_detail.html', data)
+
+
+def search(request):
+    return 'search'
